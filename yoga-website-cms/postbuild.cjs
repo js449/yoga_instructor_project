5f4c20c8d7d834c64b05cc49fa864d7b7500190e
Jagdeep singh <jsingh284@yahoo.com>
2025-07-21 19:24:53 +1200

    rem

--- yoga-website-cms/postbuild.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-const source = path.join('static', '_redirects')
-const destination = path.join('dist', '_redirects')
-
-fs.copyFileSync(source, destination)
-console.log('✅ _redirects file copied to dist/')
